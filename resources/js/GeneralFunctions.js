5a20895d6614e82f20cd6f0d4290a7f3146c6d2c
LocoNeko <loconeko73@gmail.com>
2016-04-04 17:35:18 +0900

    JS - Removed Assetic, moved JS out of layout

--- resources/js/GeneralFunctions.js
+++ resources/js/GeneralFunctions.js
@@ -1,31 +1,100 @@
+/*
+Scripts for :
+- JqueryUI : Sortable lists
+- SocketIO : reload window when 'Update' received
+- Jquery - on POST : submit FORM data as Json array
+- Jquery - on POST : include any sortable list data to the Json array
+- Jquery - on POST : Post to a URL with the 'verb' as the last part of the route
+- SocketIO - on POST : Emit 'Update'
+*/
+
+/*
+jquery code for jqueryUI to handle :
+ * - Sortable lists
+ * - Resizable (like the footer)
+ * - Draggable cards
+ * - Droppable card Slots
+ * - Droppable Senator cards
+ */
+$(document).ready(function(){
+    $('.sortable-list').sortable({
+        connectWith: '.sortable-list',
+        placeholder: 'sortable-list-placeholder'
+    });
+
+    /* Draggable with a clone as a helper :
+     * - All Senators (class sprite-Senator)
+     * - All cards with the draggable class
+     */
+    $('.sprite-Senator , .sprite-Card , .draggable').each(function() {
+        $(this).draggable({
+            zIndex: 500,
+            scroll:true,
+            start: function (event, ui) {
+               $(this).data("startingScrollTop",window.pageYOffset);
+            },
+            drag: function(event,ui){
+               var st = parseInt($(this).data("startingScrollTop"));
+               ui.position.top -= st;
+            },
+            cursor: 'move' ,
+            helper: 'clone'
+        });
+    });
+
+    // Popover
+    // - Rome Current State
+    $('#RomeCurrentState').popover({ 
+        container : 'body',
+        html : true,
+        content: function() {
+            return $('#RomeCurrentState_content').html();
+        }
+    });
+
+    $('.sprite-position-name').popover();
+
+    // jQueryUI resizable
+    // - Footer
+    $( "#footer" ).resizable({
+        handles: 'n' ,
+        alsoResizeReverse: "#main-area"
+    });
+
+});
+
+// Connecting to the socket.io server :
+// - The socket.io server is connected to a URL given as an attribute to the HTML tag 
+// - The 'Update' event triggers a reloading of the window if the realms are the same (a gameId or "Lobby")
+var socket = io.connect($("html").attr('ws_client'));
+socket.on('Update', function(realm){
+    if ( realm === $("title").attr('realm') ) {
+        window.location.reload(true) ;
+    }
+});
+
 // any submit of a form with the 'json-submit' property will result is submitting data in json to the current route + '/verb'
 // The json data will also include the data of <li> elements from any sortable list (class "sortable-list")
-$(function()
-{
-    $('.json-submit').submit(function(e)
-    {
+$(function(){
+    $('.json-submit').submit(function(e){
         //prevent Default functionality
         e.preventDefault();
 
         // Add FORM data to a json; variable made of [name] => value
         var data = $(this).serializeArray();
         var json = {};
-        $.each(data, function()
-        {
+        $.each(data, function() {
             json[this.name] = this.value || '';
         });
 
         // Add sortable lists data to the json variable in the format [list name] => json array
         // For each sortable list with an id
-        $('.sortable-list').each(function()
-        {
-            if ($(this).attr('id'))
-            {
+        $('.sortable-list').each(function() {
+            if ($(this).attr('id')) {
                 var listName = $(this).attr('id') ;
                 json[listName] = [];
                 // Go through each li tag and add it to the json [list name] array
-                $(this).find('li').each(function()
-                {
+                $(this).find('li').each(function() {
                     json[listName].push($(this).text());
                 });
             }
@@ -39,11 +108,57 @@ $(function()
         $.post(
             window.location.pathname + '/' + $(this).attr('verb') ,
             json ,
-            function( data )
-            {
+            function( data ) {
                 window.location.reload(true) ;
             } ,
             "json"
         );
     });
 });
+
+// Add a AlsoResize reverse function
+$.ui.plugin.add("resizable", "alsoResizeReverse", {
+    start: function() {
+        var that = $(this).resizable( "instance" ),
+            o = that.options;
+
+        $(o.alsoResizeReverse).each(function() {
+            var el = $(this);
+            el.data("ui-resizable-alsoresizeReverse", {
+                height: parseInt(el.height(), 10), top: parseInt(el.css("top"), 10)
+            });
+        });
+    },
+
+    resize: function(event, ui) {
+        var that = $(this).resizable( "instance" ),
+            o = that.options,
+            os = that.originalSize,
+            op = that.originalPosition,
+            delta = {
+                height: (that.size.height - os.height) || 0,
+                top: (that.position.top - op.top) || 0
+            };
+
+        $(o.alsoResizeReverse).each(function() {
+            var el = $(this), start = $(this).data("ui-resizable-alsoresize-reverse"), style = {},
+                css = el.parents(ui.originalElement[0]).length ?
+                    [ "height" ] :
+                    [ "height", "top" ];
+
+            $.each(css, function(i, prop) {
+                var sum = (start[prop] || 0) - (delta[prop] || 0);
+                if (sum && sum >= 0) {
+                    style[prop] = sum || null;
+                }
+            });
+
+            el.css(style);
+        });
+    },
+
+    stop: function() {
+        $(this).removeData("resizable-alsoresize-reverse");
+    }
+});
+
